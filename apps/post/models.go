b37ba5b01e51101069538704eb3893c6ff5ccda9
Adebayo Stephen <34807365+stevo1403@users.noreply.github.com>
2024-09-16 04:10:26 +0100

    Added Post model with foreign key constraints and fields for title, body, authorID, and author

---
+++ apps/post/models.go
@@ -0,0 +1,14 @@
+package post
+
+import (
+	"github.com/stevo1403/go-by-example/apps/user"
+	"gorm.io/gorm"
+)
+
+type Post struct {
+	gorm.Model
+	Title    string
+	Body     string
+	AuthorID int `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"` // Foreign key
+	Author   user.User
+}
