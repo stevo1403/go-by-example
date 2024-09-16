c8c8a85cd3a4747e3e1e05dbe38f945910d7de09
Adebayo Stephen <34807365+stevo1403@users.noreply.github.com>
2024-09-16 04:11:01 +0100

    Added UserSchema, UserOutSchema, UserOut, UserOutSchema, and UsersOut structs to user/schemas.go

---
+++ apps/user/schemas.go
@@ -0,0 +1,35 @@
+package user
+
+type UserSchema struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Phone     string `json:"phone"`
+	Password  string `json:"password"`
+}
+
+type UserOutSchema struct {
+	ID        uint   `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Phone     string `json:"phone"`
+}
+
+type UserOut struct {
+	User UserOutSchema `json:"user"`
+}
+
+func (u User) to_schema() UserOutSchema {
+	return UserOutSchema{
+		ID:        u.ID,
+		FirstName: u.FirstName,
+		LastName:  u.LastName,
+		Email:     u.Email,
+		Phone:     u.Phone,
+	}
+}
+
+type UsersOut struct {
+	Users []UserOutSchema `json:"users"`
+}
