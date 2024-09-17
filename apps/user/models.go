6aaa2997cf0dd2d3ead64f29aba16a8f80bac684
Adebayo Stephen <34807365+stevo1403@users.noreply.github.com>
2024-09-18 00:02:46 +0100

    Added functions to find users by ID and check if a user exists based on an ID

--- apps/user/models.go
+++ apps/user/models.go
@@ -2,6 +2,8 @@ package user
 
 import (
 	"gorm.io/gorm"
+
+	app "github.com/stevo1403/go-by-example/initializers"
 )
 
 // Create model for the user
@@ -17,4 +19,20 @@ type User struct {
 
 func (u User) GetFullName() string {
 	return u.FirstName + " " + u.LastName
+}
+
+func (u User) GetUserByID(userID int) User {
+	var user User
+	app.DB.Limit(1).First(&user, userID)
+
+	return user
+}
+
+func (u User) Exists(userID int) bool {
+	var user User
+	result := app.DB.Limit(1).First(&user, userID)
+	recordNotFound := (result.Error != nil || result.Error == gorm.ErrRecordNotFound)
+
+	return !recordNotFound
+
 }
