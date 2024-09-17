44fbc6a0109d6b229d7b30bf6d87d66a3a3913d7
Adebayo Stephen <34807365+stevo1403@users.noreply.github.com>
2024-09-17 10:18:03 +0100

    Mapped comment routes to functions

--- apps/api.go
+++ apps/api.go
@@ -2,6 +2,7 @@ package apps
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/stevo1403/go-by-example/apps/comment"
 	"github.com/stevo1403/go-by-example/apps/user"
 )
 
@@ -13,6 +14,12 @@ func LoadViews() {
 	r.GET("/users/:id", user.GetUser)
 	r.PUT("/users/:id", user.UpdateUser)
 	r.DELETE("/users/:id", user.DeleteUser)
+
+	r.GET("/comments", comment.ListComments)
+	r.POST("/comments/", comment.CreateComment)
+	r.GET("/comments/:id", comment.GetComment)
+	r.PUT("/comments/:id", comment.UpdateComment)
+	r.DELETE("/comments/:id", comment.DeleteComment)
 
 	r.Run()
 }
