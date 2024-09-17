e100d6c03f4ba81e73f6ad63b1a94e4bd2959f81
Adebayo Stephen <34807365+stevo1403@users.noreply.github.com>
2024-09-18 00:00:45 +0100

    Added a function to update the Author and Post fields of Comment objects

--- apps/comment/models.go
+++ apps/comment/models.go
@@ -3,6 +3,7 @@ package comment
 import (
 	"github.com/stevo1403/go-by-example/apps/post"
 	"github.com/stevo1403/go-by-example/apps/user"
+	app "github.com/stevo1403/go-by-example/initializers"
 	"gorm.io/gorm"
 )
 
@@ -13,6 +14,16 @@ type Comment struct {
 	PostID    int `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"` // Foreign key
 	Post      post.Post
 	Body      string
+	UpVotes   int
+	DownVotes int
+}
+
+func (c Comment) UpdateFields() {
+	app.DB.Limit(1).First(&c.Author, c.AuthorID)
+	app.DB.Limit(1).First(&c.Post, c.PostID)
+}
+
+type Votes struct {
 	UpVotes   int
 	DownVotes int
 }
