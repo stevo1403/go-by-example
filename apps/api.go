7d1ee7e7ff2e61ca2692f04894620ba558fe1b9e
Adebayo Stephen <34807365+stevo1403@users.noreply.github.com>
2024-09-17 23:57:48 +0100

    Added routing for Post routes

--- apps/api.go
+++ apps/api.go
@@ -3,6 +3,7 @@ package apps
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/stevo1403/go-by-example/apps/comment"
+	"github.com/stevo1403/go-by-example/apps/post"
 	"github.com/stevo1403/go-by-example/apps/user"
 )
 
@@ -19,7 +20,15 @@ func LoadViews() {
 	r.POST("/comments/", comment.CreateComment)
 	r.GET("/comments/:id", comment.GetComment)
 	r.PUT("/comments/:id", comment.UpdateComment)
+	r.PATCH("/comments/:id/upvote", comment.UpvoteComment)
+	r.PATCH("/comments/:id/downvote", comment.DownVoteComment)
 	r.DELETE("/comments/:id", comment.DeleteComment)
+
+	r.GET("/posts", post.ListPosts)
+	r.POST("/posts", post.CreatePost)
+	r.GET("/posts/:id", post.GetPost)
+	r.PUT("/posts/:id", post.UpdatePost)
+	r.DELETE("/posts/:id", post.DeletePost)
 
 	r.Run()
 }
