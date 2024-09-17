b48b751b81e40f3884c6c183ef5ddf3ee37e25ab
Adebayo Stephen <34807365+stevo1403@users.noreply.github.com>
2024-09-17 23:56:05 +0100

    Modified 'initializers' package import to use 'app' alias

--- app.go
+++ app.go
@@ -4,17 +4,17 @@ import (
 	"github.com/stevo1403/go-by-example/apps/comment"
 	"github.com/stevo1403/go-by-example/apps/post"
 	"github.com/stevo1403/go-by-example/apps/user"
-	"github.com/stevo1403/go-by-example/initializers"
+	app "github.com/stevo1403/go-by-example/initializers"
 )
 
 func Migrate() {
-	initializers.DB.AutoMigrate(&user.User{})
-	initializers.DB.AutoMigrate(&post.Post{})
-	initializers.DB.AutoMigrate(&comment.Comment{})
+	app.DB.AutoMigrate(&user.User{})
+	app.DB.AutoMigrate(&post.Post{})
+	app.DB.AutoMigrate(&comment.Comment{})
 }
 
 func Load() {
-	initializers.LoadEnvVariables()
-	initializers.LoadDB()
+	app.LoadEnvVariables()
+	app.LoadDB()
 	Migrate()
 }
