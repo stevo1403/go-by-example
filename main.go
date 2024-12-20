4bf4aabf54c60df456d9671b10c6fed5ad17d7b7
Adebayo Stephen <34807365+stevo1403@users.noreply.github.com>
2024-12-20 05:18:22 +0100

    feat: Add OpenAPI Operations annotations for the UnGo API including security definitions

--- main.go
+++ main.go
@@ -8,6 +8,20 @@ func init() {
 	Load()
 }
 
+// @title UnGo API
+// @version 1.0
+// @description This is a simple API for a blogging platform
+// @BasePath /api/v1
+// @host localhost:8080
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+// @description "Bearer token for API authorization"
+// @description Type "Bearer" followed by a space and JWT token.
+// @schemes http
+// @produce json
+// @consumes json
+// externalDocs.description OpenAPI
 func main() {
 	apps.LoadViews()
 }
