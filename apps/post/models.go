4f5270efd5cff7c5fd7ef3edb0efddc95f57d58f
Adebayo Stephen <34807365+stevo1403@users.noreply.github.com>
2025-01-29 13:05:26 +0100

    feat: Enhance Post model with views tracking, publishing functionality, and mutex for safe view increments

--- apps/post/models.go
+++ apps/post/models.go
@@ -1,6 +1,10 @@
 package post
 
 import (
+	"fmt"
+	"sync"
+	"time"
+
 	"gorm.io/gorm"
 
 	"github.com/stevo1403/go-by-example/apps/user"
@@ -9,17 +13,28 @@ import (
 
 type Post struct {
 	gorm.Model
-	Title    string
-	Body     string
-	AuthorID uint      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"` // Foreign key
-	Author   user.User // Todo: Change this to allow multiple author per posts
+	Title       string     // Title of the post
+	Body        string     // Body of the post
+	Views       uint64     // Number of views
+	IsDraft     bool       `gorm:"default:false"` // Is the post a draft, should be false by default
+	PublishedAt time.Time  // Date the post was published
+	AuthorID    uint       `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"` // Foreign key
+	Author      user.User  // Todo: Change this to allow multiple author per posts
+	mu          sync.Mutex // Mutex for incrementing views
 }
 
-func (p Post) UpdateFields() {
+type PostViews struct {
+	gorm.Model
+	PostID uint // Foreign key for Post
+	Post
+	Viewers []user.User `gorm:"many2many:post_view_users;"` // Many-to-many relationship with User
+}
+
+func (p *Post) UpdateFields() {
 	app.DB.Limit(1).First(&p.Author, p.AuthorID)
 }
 
-func (p Post) Exists(postID uint) bool {
+func (p *Post) Exists(postID uint) bool {
 	// Checks if post exists
 	var post Post
 	result := app.DB.Limit(1).First(&post, postID)
@@ -27,4 +42,34 @@ func (p Post) Exists(postID uint) bool {
 
 	return !recordNotFound
 
+}
+
+// Publish the post
+func (p *Post) Publish() {
+	if p.IsDraft {
+		p.IsDraft = false
+	} else {
+		p.PublishedAt = time.Now()
+	}
+}
+
+// IncrementViews safely increments the Views field
+func (p *Post) IncrementViews(Viewer user.User) {
+	var postViews PostViews
+	isViewed := app.DB.Limit(1).
+		Where(map[string]interface{}{"post_id": p.ID, "author_id": Viewer.ID}).
+		First(&postViews).
+		Error == nil
+	if isViewed {
+		return
+	}
+
+	p.mu.Lock() // Lock the mutex
+	defer p.mu.Unlock()
+
+	p.Views++                                                                   // Increment the views
+	fmt.Println("Incrementing views for post with ID: ", p.ID, " to ", p.Views) // Log the increment
+	app.DB.FirstOrCreate(&postViews, PostViews{Post: *p, PostID: p.ID})         // Create a new record if it does not exist
+	postViews.Viewers = append(postViews.Viewers, Viewer)                       // Append the viewer to the viewers list
+	app.DB.Save(&postViews)                                                     // Save the post views
 }
