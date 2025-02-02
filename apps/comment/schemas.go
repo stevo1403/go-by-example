b62ccf4bc6ed92721bc21f0d47bf2c61913c9d44
Adebayo Stephen <34807365+stevo1403@users.noreply.github.com>
2025-02-02 12:45:07 +0100

    feat: Add `CommentCreateSchema` and include `PublishedAt` field in `CommentOutSchema`. Add a `from_schema` function to `CommentCreateSchema`.

--- apps/comment/schemas.go
+++ apps/comment/schemas.go
@@ -8,14 +8,21 @@ type CommentSchema struct {
 	DownVotes uint64 `json:"downvotes"`
 }
 
+type CommentCreateSchema struct {
+	AuthorID uint   `json:"author_id"`
+	PostID   uint   `json:"post_id"`
+	Body     string `json:"body"`
+}
+
 type CommentOutSchema struct {
-	ID         uint   `json:"id"`
-	AuthorID   uint   `json:"author_id"`
-	AuthorName string `json:"author_name"`
-	PostID     uint   `json:"post_id"`
-	Body       string `json:"body"`
-	UpVotes    uint64 `json:"upvotes"`
-	DownVotes  uint64 `json:"downvotes"`
+	ID          uint   `json:"id"`
+	AuthorID    uint   `json:"author_id"`
+	AuthorName  string `json:"author_name"`
+	PostID      uint   `json:"post_id"`
+	Body        string `json:"body"`
+	UpVotes     uint64 `json:"upvotes"`
+	DownVotes   uint64 `json:"downvotes"`
+	PublishedAt string `json:"published_at"`
 }
 
 type CommentOut struct {
@@ -25,18 +32,28 @@ type CommentOut struct {
 // Converts a `Comment“ object to a `CommentOutSchema“ object
 func (c Comment) to_schema() CommentOutSchema {
 	return CommentOutSchema{
-		ID:         c.ID,
-		AuthorID:   c.AuthorID,
-		AuthorName: c.Author.GetUserByID(c.AuthorID).GetFullName(),
-		PostID:     c.PostID,
-		Body:       c.Body,
-		UpVotes:    c.UpVotes,
-		DownVotes:  c.DownVotes,
+		ID:          c.ID,
+		AuthorID:    c.AuthorID,
+		AuthorName:  c.Author.GetUserByID(c.AuthorID).GetFullName(),
+		PostID:      c.PostID,
+		Body:        c.Body,
+		UpVotes:     c.UpVotes,
+		DownVotes:   c.DownVotes,
+		PublishedAt: c.CreatedAt.Format("2006-01-02 15:04:05"),
 	}
 }
 
 // Convert `CommentSchema` to `Comment` object
 func (_s *CommentSchema) from_schema() Comment {
+	return Comment{
+		AuthorID: _s.AuthorID,
+		PostID:   _s.PostID,
+		Body:     _s.Body,
+	}
+}
+
+// Convert `CommentCreateSchema` to `Comment` object
+func (_s *CommentCreateSchema) from_schema() Comment {
 	return Comment{
 		AuthorID: _s.AuthorID,
 		PostID:   _s.PostID,
